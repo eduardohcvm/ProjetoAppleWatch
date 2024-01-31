3536e119e32b469aad55b222839883a94120d1bc
Eduardo Henrique <eduardohcvm@gmail.com>
2024-01-31 14:32:35 -0300

    second

---
+++ applewatch.js
@@ -0,0 +1,23 @@
+const verdeCipreste ={
+    nome: 'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+};
+const azulInverno = {
+    nome: 'Azul-inverno',
+    pasta: 'imagens-azul-inverno'
+};
+const meiaNoite = {
+    nome: "Meia-noite",
+    pasta: 'imagens-meia-noite'
+};
+
+const estelar = {
+    nome: 'Estelar',
+    pasta: 'imagens-estelar'
+};
+
+const rosaClaro = { 
+    none: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+};
+
